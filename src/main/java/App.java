cdc938c93e290c5e2dae99af7136ff7937ecce93
ttamttam <ttamttom0@gmail.com>
2024-07-16 17:25:58 +0400

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.getMessage());
+
+        System.out.println("bean1 == bean2: " + (bean1 == bean2));
+        System.out.println("cat1 == cat2: " + (cat1 == cat2));
     }
 }
